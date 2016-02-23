5184ba2315acf29c635c75e08dc943b7a3f2e38c
Publicover <jameslpublicover@gmail.com>
2016-02-23 13:18:10 -0500

    create voter class

---
+++ db/migrate/20160223181733_create_voters.rb
@@ -0,0 +1,13 @@
+class CreateVoters < ActiveRecord::Migration
+  def change
+    create_table :voters do |t|
+      t.integer :id
+      t.integer :candidate_id
+      t.string :name
+      t.string :party
+      t.string :token
+
+      t.timestamps null: false
+    end
+  end
+end
