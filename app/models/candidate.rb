bb84982dab4186b8ce6c228817f65b5fd2b63edb
Publicover <jameslpublicover@gmail.com>
2016-02-23 13:25:29 -0500

    create associations and validations

--- app/models/candidate.rb
+++ app/models/candidate.rb
@@ -1,2 +1,9 @@
 class Candidate < ActiveRecord::Base
+  has_many :voters
+  has_many :votes
+
+  validates :name, presence: true
+  validates :hometown, presence: true
+  validates :district, presence: true
+  validates :party, presence: true
 end
--- app/models/voter.rb
+++ app/models/voter.rb
@@ -1,2 +1,8 @@
 class Voter < ActiveRecord::Base
+  has_one :vote
+  belongs_to :candidate
+
+  validates :candidate_id, presence: true
+  validates :name, presence: true
+  validates :party, presence: true
 end
