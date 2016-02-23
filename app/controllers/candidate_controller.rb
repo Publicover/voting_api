5178de57ea7cc206a7c9ea6159272a9c9092945b
Publicover <jameslpublicover@gmail.com>
2016-02-23 15:38:32 -0500

    mess up creating through the URL, then create like seven at once. it works now, though

--- app/controllers/candidate_controller.rb
+++ app/controllers/candidate_controller.rb
@@ -9,6 +9,10 @@ class CandidateController < ApplicationController
   end
 
   def create
-    Candidate.create
+    name = params["name"]
+    hometown = params["hometown"]
+    district = params["district"]
+    party = params["party"]
+    Candidate.create(name: name, hometown: hometown, district: district, party: party)
   end
 end
